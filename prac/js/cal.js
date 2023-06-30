f079b39da98134383c08fa45f647529c3e0a8c88
Gun-0927 <bu1029@naver.com>
2023-06-30 14:19:33 +0900

    WebStudy!

---
+++ prac/js/cal.js
@@ -0,0 +1,36 @@
+function display(val){
+    document.getElementById('result').value += val;
+    return val;
+}
+
+function solve(){
+    let x = document.getElementById('result').value;
+    let y = eval(x);
+    document.getElementById('result').value = y;
+    return y;
+}
+
+function clearScreen(){
+    document.getElementById('result').value = '';
+}
+//let f = document.querySelector('.keys').firstElementChild;
+//f.onclick=function(){display(f.value);};
+window.onload=function(){
+    let buttons=document.getElementsByClassName('button');
+    let operators=document.getElementsByClassName('operator');
+    for(let button of buttons){
+        let val=button.value;
+        if(!button.className.includes('equal-sign')){
+            button.addEventListener('click',()=>display(val));
+        }
+    }
+    for(let oper of operators){
+        let val=oper.value;
+        oper.addEventListener('click',()=>display(val));
+    }
+    document.querySelector('.equal-sign')
+        .addEventListener('click',()=>solve());
+}
+
+//window.onload documnet.onload
+//(function(){})();
